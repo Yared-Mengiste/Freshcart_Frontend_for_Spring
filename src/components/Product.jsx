692168ccfcfe11fa330c93f90073408546b5844b
Yared Mengiste <yaredmengiste01@gmail.com>
2026-03-11 18:02:09 +0300

    Fix casing of product.css import in Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@
 
 import React, {useState} from "react";
 import  {useNavigate} from 'react-router-dom'
-import "./Product.css";
+import "./product.css";
 import Plus from "../assets/plus.png";
 import { useMessage } from "../context/MessageContext";
 
